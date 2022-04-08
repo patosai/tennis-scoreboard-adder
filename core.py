9f85a2661a89babdf74e58422725adb809cc8ac1
Patrick Tsai <patosai@users.noreply.github.com>
2022-04-08 01:47:10 -0400

    Text labels appearing

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from moviepy.editor import *
 import json
 
@@ -5,14 +7,13 @@ import json
 def validate_score_json(scores):
     assert isinstance(scores, list)
     for score in scores:
-        assert score['timestamp'], "Needs a timestamp"
+        assert 'timestamp' in score, "Needs a timestamp"
         assert isinstance(score['player1_score'], list), "player1_score needs to be a list"
         assert isinstance(score['player2_score'], list), "player2_score needs to be a list"
         assert len(score['player1_score']) == len(score['player2_score']), "player scores need to be the same length"
         if len(score['player1_score']) > 0:
             assert score['player1_score'][-1] in [0, 15, 30, 40, "AD"], "invalid player1 score: %s" % score['player1_score'][-1]
             assert score['player2_score'][-1] in [0, 15, 30, 40, "AD"], "invalid player2 score: %s" % score['player2_score'][-1]
-    scores.sort(key=lambda x: x['timestamp'])
     return scores
 
 
@@ -21,3 +22,16 @@ def parse_score_file(filename):
         scores = json.load(file)
     validate_score_json(scores)
     return scores
+
+
+def add_scores_to_video(scores, video_filename):
+    scores.sort(key=lambda x: x['timestamp'])
+    clip = VideoFileClip(video_filename)
+    my_name_text = TextClip("Patrick", fontsize=24, color='white', font="Helvetica Neue").set_position((0.03, 0.9), relative=True).set_duration(10)
+    opponent_text = TextClip("Opponent", fontsize=24, color='white', font="Helvetica Neue").set_position((0.03, 0.94), relative=True).set_duration(10)
+    video = CompositeVideoClip([clip, my_name_text, opponent_text])
+    video.preview()
+
+
+if __name__ == "__main__":
+    add_scores_to_video([], "/Users/p/Desktop/4-3-2022.mov")
--- test.py
+++ test.py
@@ -5,12 +5,19 @@ from . import core
 
 class ScoreFileParserTest(unittest.TestCase):
     def test_validate_score_json(self):
-        good_case = [{'timestamp': 10.3, 'player1_score': [6, 4, 15], 'player2_score': [3, 2, 30]}]
-        self.assertEqual(good_case, core.validate_score_json(good_case))
+        good_case_two_sets = [{'timestamp': 10.3, 'player1_score': [6, 4, 15], 'player2_score': [3, 2, 30]}]
+        self.assertEqual(good_case_two_sets, core.validate_score_json(good_case_two_sets))
+
+        good_case_no_sets = [{'timestamp': 10.89, 'player1_score': [15], 'player2_score': [30]}]
+        self.assertEqual(good_case_no_sets, core.validate_score_json(good_case_no_sets))
 
         bad_game_score = [{'timestamp': 10.3, 'player1_score': [6, 4, 15], 'player2_score': [3, 2, 31]}]
         with self.assertRaises(AssertionError):
             core.validate_score_json(bad_game_score)
+
+        no_timestamp = [{'player1_score': [2, 15], 'player2_score': [5, 40]}]
+        with self.assertRaises(AssertionError):
+            core.validate_score_json(no_timestamp)
 
 
 if __name__ == '__main__':
